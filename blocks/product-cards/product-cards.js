eb4625f641206feb1818961f2794b50b46da5eb8
timwlee <timothyl@adobe.com>
2025-10-27 12:56:17 -0700

    Update URL in products sheet

--- blocks/product-cards/product-cards.js
+++ blocks/product-cards/product-cards.js
@@ -1,7 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  const products = 'https://main--demo-boilerplate--lamontacrook.hlx.page/misc/products.json?sheet=tory-burch';
+  const products = 'https://main--demo-boilerplate--lamontacrook.hlx.page/misc/products.json?sheet=tommy-bahama';
 
   const lastDiv = [...block.children].pop();
   let skus = lastDiv.querySelector('p');
