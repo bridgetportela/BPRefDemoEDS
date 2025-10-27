b4b8cd9b1fba017d98f2b47054329af35495bb17
timwlee <timothyl@adobe.com>
2025-10-27 12:12:48 -0700

    Add product-cards block

---
+++ blocks/product-cards/product-cards.js
@@ -0,0 +1,32 @@
+import { createOptimizedPicture } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  const products = 'https://main--demo-boilerplate--lamontacrook.hlx.page/misc/products.json?sheet=tory-burch';
+
+  const lastDiv = [...block.children].pop();
+  let skus = lastDiv.querySelector('p');
+  skus = skus ? skus.textContent.split(',') : [''];
+  const ul = document.createElement('ul');
+
+  const productsResponse = await fetch(products);
+  if (productsResponse.ok) {
+    const p = await productsResponse.json();
+    console.log(p);
+    p.data.forEach(product => skus.forEach(sku => {
+      if (product.SKU === sku) {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        div.className = 'product-card-image';
+        // const optimizedPic = createOptimizedPicture(product['Image {width:80}'], 'DM Image', false, [{ width: '750' }]);
+        div.innerHTML = `<img src="${product['Image {width:80}']}" alt="${product['Product Name']}">`;
+        const p = document.createElement('p');
+        p.textContent = product['Product Name'];
+        li.append(div, p);
+        ul.append(li);
+      }
+    }));
+  }
+  const place =lastDiv.querySelector('div');
+  place.append(ul);
+  // block.append(ul);
+}
